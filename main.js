54a4a442c68a16e6b4bc73aa49ba6e5f260bf9b5
cjd <carljduff@gmail.com>
2025-07-24 15:09:46 -0400

    files created

---
+++ main.js
@@ -0,0 +1,5 @@
+// const rootDiv = document.getElementById("root");
+const keyboardDiv = document.getElementById("keyboard-container");
+const wordDiv = document.getElementById("word-container");
+const guessDiv = document.getElementById("guess-container");
+
